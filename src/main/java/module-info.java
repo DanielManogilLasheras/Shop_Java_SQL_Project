ef22f04b233d5b14533839161a8a766a50f5b109
Dan94A <kwyrlx@gmail.com>
2024-08-23 17:36:25 +0200

    Initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.shop.leinadprojects.shop_java_sql {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.shop.leinadprojects.shop_java_sql to javafx.fxml;
+    exports com.shop.leinadprojects.shop_java_sql;
+}
